eed773c72b1bfdb74859a4b38956c087cb123035
3elk4 <izaa1109@gmail.com>
2023-08-02 20:41:41 +0200

    add helper

---
+++ DndCharCreator/DndCharCreator/Helper/WindowHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DndCharCreator.Helper
+{
+    static class WindowHelper
+    {
+        public static void SwapWindows(Window window)
+        {
+            App.Current.MainWindow.Close();
+            App.Current.MainWindow = window;
+        }
+    }
+}
