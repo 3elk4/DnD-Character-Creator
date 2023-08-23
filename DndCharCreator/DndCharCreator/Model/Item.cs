6156e7af9893f717f4173719b6b1d6ac2e9e8402
3elk4 <izaa1109@gmail.com>
2023-08-23 19:11:23 +0200

    add item model

---
+++ DndCharCreator/DndCharCreator/Model/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DndCharCreator.Model
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public bool Exists { get; set; }
+        public int Quantity { get; set; }
+        public double Weight { get; set; }
+
+        public Item()
+        {
+            this.Exists = true;
+        }
+    }
+}
