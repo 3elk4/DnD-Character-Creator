3af3909371b85296af8088213d18870f524142f2
3elk4 <izaa1109@gmail.com>
2023-08-25 11:51:42 +0200

    add feat model

---
+++ DndCharCreator/DndCharCreator/Model/Feat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DndCharCreator.Model
+{
+    public class Feat
+    {
+        public string Title { get; set; }
+        public string Source { get; set; }
+        public string SourceType { get; set; }
+        public string Definition { get; set; }
+    }
+}
